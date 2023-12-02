f4ff3f903a32d3ed5bc036a5c96e7f39379d2020
Josh Lartz <jlartz@cvent.com>
2023-12-02 10:50:02 -0600

    day 1 part 2

--- src/day01.rs
+++ src/day01.rs
@@ -1,32 +1,75 @@
 use std::str;
 
-type Input = Vec<String>;
+type Input<'a> = Vec<&'a str>;
 
 pub fn generator(input: &str) -> Input {
-    input
-        .split('\n')
-        .map(|line| line.chars().filter(|char| char.is_numeric()).collect())
-        .collect()
+    input.split('\n').collect()
 }
 
 pub fn part1(input: &Input) -> u32 {
     input
         .iter()
         .map(|line| {
-            let mut chars = line.chars();
-            let mut two_chars = String::from(chars.next().unwrap());
-            two_chars.push(if line.len() == 1 {
-                line.parse::<char>().unwrap()
-            } else {
-                chars.last().unwrap()
-            });
-            two_chars.parse::<u32>().unwrap()
+            let numbers = line.chars().filter(|char| char.is_numeric()).collect();
+            combine_numbers(numbers)
         })
         .sum()
 }
 
 pub fn part2(input: &Input) -> u32 {
-    part1(input)
+    input
+        .iter()
+        .map(|line| {
+            let numbers = find_numbers(line);
+            combine_numbers(numbers)
+        })
+        .sum()
+}
+
+fn combine_numbers(numbers: Vec<char>) -> u32 {
+    let mut numbers = numbers.iter();
+    let mut two_chars = String::from(*numbers.next().unwrap());
+    two_chars.push(if let Some(c) = numbers.last() {
+        *c
+    } else {
+        two_chars.parse::<char>().unwrap()
+    });
+    two_chars.parse::<u32>().unwrap()
+}
+
+struct FoundNumber {
+    index: usize,
+    number: char,
+}
+
+fn find_numbers(line: &str) -> Vec<char> {
+    let mut found_numbers: Vec<FoundNumber> = Vec::new();
+
+    let mut find_number = |input: &str, number: char| {
+        line.match_indices(input)
+            .for_each(|(index, _)| found_numbers.push(FoundNumber { index, number }));
+    };
+
+    find_number("one", '1');
+    find_number("two", '2');
+    find_number("three", '3');
+    find_number("four", '4');
+    find_number("five", '5');
+    find_number("six", '6');
+    find_number("seven", '7');
+    find_number("eight", '8');
+    find_number("nine", '9');
+
+    line.match_indices(|ch| ('1'..='9').contains(&ch))
+        .for_each(|(index, number)| {
+            found_numbers.push(FoundNumber {
+                index,
+                number: number.chars().next().unwrap(),
+            })
+        });
+
+    found_numbers.sort_by(|a, b| a.index.cmp(&b.index));
+    found_numbers.iter().map(|each| each.number).collect()
 }
 
 #[cfg(test)]
